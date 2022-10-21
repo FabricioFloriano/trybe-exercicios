3c9c11430ec51361fc670d055c9bfd0b8db181f2
FabricioFloriano <f.florian0@hotmail.com>
2022-10-21 16:44:22 -0300

    exemplos e exercicios do corse

---
+++ fundamentos/Introdução-a-JavaScript/D2/script.js
@@ -0,0 +1,110 @@
+// Exemplo 1 - <<<<<<<<<<<<<<<<<<<<<<<<<
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// console.log(tasksList.length);
+// // 3
+
+// Exemplo 2 - <<<<<<<<<<<<<<<<<<<<<<<<<
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// let searchForFirstTask = tasksList[0];
+// console.log(searchForFirstTask);
+// // Tomar café
+
+// let searchForLastTask = tasksList[tasksList.length - 1];
+// console.log(searchForLastTask);
+// // Brincar com o cachorro
+
+/// >>>>>>>>>>obs:Desse modo, para acessarmos o último elemento da estrutura, devemos pegar a quantidade de seus elementos utilizando o .length 
+// e subtrair 1.<<<<
+
+// Exemplo 3 - <<<<<<<<<<<<<<<<<<<<<<<<<
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+// console.log(tasksList);
+
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+// >>>>>>>>Obs: Se precisarmos adicionar no início, podemos usar o .unshift().<<<<<<
+
+// Exemplo 4 - <<<<<<<<<<<<<<<<<<<<<<<<<
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// tasksList.pop();  // remove a última tarefa
+// console.log(tasksList);
+
+// [ 'Tomar café', 'Reunião' ]
+
+//>>>>>>>>>>>>>>>>> Obs: O método .pop() nos permite remover o último item do array. Mas se precisarmos remover o primeiro item da lista, podemos usar o .shift(). Faça em seu console e veja o resultado.<<<<<<<<<<<<<
+
+// Exemplo 5 - <<<<<<<<<<<<<<<<<<<<<<<<<
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// let indexOfTask = tasksList.indexOf('Reunião');
+// console.log(indexOfTask);
+
+
+
+// Exercicio 1 <<<<<
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// Exercicio 2 <<<<<
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu [2];
+
+// console.log(indexOfPortfolio);
+
+// Exercicio 3 <<<<<
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+// menu.push('Contato');
+
+// console.log(menu);
+
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+// For
+
+// Exemplo 1 - <<<<<<<<<<<<<<<<<<<<<<<<<
+
+// var numero = 7; 
+
+// for (var contador = 1; contador<= 9; contador ++){
+//     console.log(numero * contador);
+// }
+
+// Exemplo 2 - <<<<<<<<<<<<<<<<<<<<<<<<<
+
+// let cars = ["Saab", "Volvo", "BMW"];
+
+// for (let index = 0; index < cars.length; index += 1) {
+//   console.log(cars[index]);
+// }
+
+
+
+// Exercicio 1 <<<<<
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for (let lista = 0; lista < groceryList.length; lista = lista + 1) {
+//     const list = groceryList[lista]
+//     console.log(list);
+// }
+
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+
+
