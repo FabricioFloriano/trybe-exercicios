f3bcdb941fbaf0fd099228dde331e6a41dafa8ac
FabricioFloriano <f.florian0@hotmail.com>
2022-10-26 21:30:26 -0300

    Terminado

--- fundamentos/Introdução-a-JavaScript/Exercicios-4.5.js
+++ fundamentos/Introdução-a-JavaScript/Exercicios-4.5.js
@@ -1,2 +1,58 @@
 // Iniciando exercicios fundamentos M4.d5
 
+    // // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
+    // let pessoa = {
+    //     nome: 'Henri',
+    //     idade: 20
+    // }
+    //   pessoa = {
+    //     nome: 'Luna',
+    //     idade: 19
+    // } // Altere essa estrutura para corrigir o erro.
+    //   console.log('Nome:', pessoa.nome);
+    //   console.log('Idade:', pessoa.idade);
+
+    // 2 -- Exercicios
+
+    // function numeroAleatorio() {
+    //     return Math.random()
+    //   }
+    //   console.log(numeroAleatorio());
+
+    //   const numeroAleatorio = () => Math.random()
+
+    //   console.log(numeroAleatorio());
+
+    // 3 -- Exercicios
+
+    // function hello(nome) {
+    //     return `Olá, ${nome}!`
+    //   }
+    //   let nome = 'Ivan';
+    //   console.log(hello(nome));
+
+    //   const hello = () => `Olá ${nome}!`
+    //   let nome = 'Ivan'
+      
+    // //   console.log(hello());
+
+        // 4 -- Exercicios
+
+        // const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`
+
+        //   let nome = 'Ivan';
+        //   let sobrenome = 'Pires';
+
+        //   console.log(nomeCompleto(nome, sobrenome)); 
+
+ // 5 -- Exercicios
+ 
+//  let speed = 90;
+
+//  const speedCar = (speed) => (
+//    speed >= 120 ? `Você ultrapassou o limite de velocidade`:`Você está na velocidade permitida`
+//    );
+ 
+//  console.log(speedCar(speed));
+
+// 6 -- Exercicios
